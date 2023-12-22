550f2722ecdd32c48a2d3965aac9d8210592d98b
Ashish Mourya <ashishmourya@Ashishs-MacBook-Air.local>
2023-12-22 14:43:13 +0530

    shopping_cart_modified

--- src/test/java/com/demo/shopping/controller/UserControllerTest.java
+++ src/test/java/com/demo/shopping/controller/UserControllerTest.java
@@ -1,67 +1,67 @@
-package com.demo.shopping.controller;
-
-import com.demo.shopping.dto.UserDto;
-import com.demo.shopping.model.User;
-import com.demo.shopping.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-public class UserControllerTest {
-    @MockBean
-    private UserService userService;
-
-    @Autowired
-    private ModelMapper mapper;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    UserDto user;
-
-    @BeforeEach
-    public void init(){
-        user = UserDto.builder()
-                .name("Amith")
-                .email("amit@gmail.com")
-                .date_of_birth(null)
-                .password("12345").build();
-
-    }
-
-    @Test
-    public void createUserTest() throws Exception{
-        Mockito.when(userService.createUser(Mockito.any())).thenReturn(user);
-
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(convertObjectToJsonString(user)))
-                        .andReturn().getResponse().getStatus();
-
-
-    }
-
-
-    private String convertObjectToJsonString(Object user){
-        try{
-            return new ObjectMapper().writeValueAsString(user);
-
-        }catch(Exception e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-
-}
+//package com.demo.shopping.controller;
+//
+//import com.demo.shopping.dto.UserDto;
+//import com.demo.shopping.model.User;
+//import com.demo.shopping.service.UserService;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mockito;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class UserControllerTest {
+//    @MockBean
+//    private UserService userService;
+//
+//    @Autowired
+//    private ModelMapper mapper;
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    UserDto user;
+//
+//    @BeforeEach
+//    public void init(){
+//        user = UserDto.builder()
+//                .name("Amith")
+//                .email("amit@gmail.com")
+//                .date_of_birth(null)
+//                .password("12345").build();
+//
+//    }
+//
+//    @Test
+//    public void createUserTest() throws Exception{
+//        Mockito.when(userService.createUser(Mockito.any())).thenReturn(user);
+//
+//        this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(convertObjectToJsonString(user)))
+//                        .andReturn().getResponse().getStatus();
+//
+//
+//    }
+//
+//
+//    private String convertObjectToJsonString(Object user){
+//        try{
+//            return new ObjectMapper().writeValueAsString(user);
+//
+//        }catch(Exception e){
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+//
+//
+//}
--- src/test/java/com/demo/shopping/service/UserServiceTest.java
+++ src/test/java/com/demo/shopping/service/UserServiceTest.java
@@ -1,80 +1,80 @@
-package com.demo.shopping.service;
-
-import com.demo.shopping.dto.PageableResponse;
-import com.demo.shopping.dto.UserDto;
-import com.demo.shopping.model.User;
-import com.demo.shopping.repositories.UserRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-
-@SpringBootTest
-public class UserServiceTest {
-    @Autowired
-    private ModelMapper mapper;
-
-    @MockBean
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserService userService;
-
-    User user;
-    @BeforeEach
-    public void init(){
-        user = User.builder()
-                .name("Amith")
-               .email("amit@gmail.com")
-               .date_of_birth(null)
-               .password("12345").build();
-
-    }
-
-    @Test
-    public void createUserTest(){
-        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
-        UserDto user1= userService.createUser(mapper.map(user, UserDto.class));
-        Assertions.assertNotNull(user1);
-
-    }
-
-    @Test
-    public void getAllUserTest(){
-        User user1 = User.builder()
-                .name("Amith")
-                .email("amit@gmail.com")
-                .date_of_birth(null)
-                .password("12345").build();
-
-        User user2 = User.builder()
-                .name("Amith")
-                .email("amit@gmail.com")
-                .date_of_birth(null)
-                .password("12345").build();
-
-
-        List<User> userList = Arrays.asList(user,user1,user2);
-        Page<User> page = new PageImpl<>(userList);
-        Mockito.when(userRepository.findAll((Pageable) Mockito.any())).thenReturn(page);
-
-        PageableResponse<UserDto> allUser = userService.getAllUser(1,2,"name","asc");
-        Assertions.assertEquals(3,allUser.getContent().size());
-
-
-    }
-
-
-
-}
+//package com.demo.shopping.service;
+//
+//import com.demo.shopping.dto.PageableResponse;
+//import com.demo.shopping.dto.UserDto;
+//import com.demo.shopping.model.User;
+//import com.demo.shopping.repositories.UserRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mockito;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageImpl;
+//import org.springframework.data.domain.Pageable;
+//
+//import java.time.LocalDate;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@SpringBootTest
+//public class UserServiceTest {
+//    @Autowired
+//    private ModelMapper mapper;
+//
+//    @MockBean
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    User user;
+//    @BeforeEach
+//    public void init(){
+//        user = User.builder()
+//                .name("Amith")
+//               .email("amit@gmail.com")
+//               .date_of_birth(null)
+//               .password("12345").build();
+//
+//    }
+//
+//    @Test
+//    public void createUserTest(){
+//        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+//        UserDto user1= userService.createUser(mapper.map(user, UserDto.class));
+//        Assertions.assertNotNull(user1);
+//
+//    }
+//
+//    @Test
+//    public void getAllUserTest(){
+//        User user1 = User.builder()
+//                .name("Amith")
+//                .email("amit@gmail.com")
+//                .date_of_birth(null)
+//                .password("12345").build();
+//
+//        User user2 = User.builder()
+//                .name("Amith")
+//                .email("amit@gmail.com")
+//                .date_of_birth(null)
+//                .password("12345").build();
+//
+//
+//        List<User> userList = Arrays.asList(user,user1,user2);
+//        Page<User> page = new PageImpl<>(userList);
+//        Mockito.when(userRepository.findAll((Pageable) Mockito.any())).thenReturn(page);
+//
+//        PageableResponse<UserDto> allUser = userService.getAllUser(1,2,"name","asc");
+//        Assertions.assertEquals(3,allUser.getContent().size());
+//
+//
+//    }
+//
+//
+//
+//}
